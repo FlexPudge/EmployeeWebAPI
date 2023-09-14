dce9b409a124b7fdab2116124f5934f2350fb15c
FlexPudge <micha.parmuzin@yandex.ru>
2023-09-14 15:53:28 +0300

    EditLaunchSetting

--- Program.cs
+++ Program.cs
@@ -3,16 +3,12 @@ using WebApi.Repositorys;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
 builder.Services.AddDbContext<InMemoryDbContext>();
 builder.Services.AddScoped<IEmployeeRepository, EmployeeEFRepository>();
 builder.Services.AddScoped<IAddressRepository, AddressEFRepository>();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
 
